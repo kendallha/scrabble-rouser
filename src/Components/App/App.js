7167c2a1c8553f7027868b5f1f6c26f5174dd597
kendallha <kendallhaw@gmail.com>
2021-06-08 17:58:51 -0700

    Add state to App and add routes for SavedWords and TopScorer components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,33 @@
 
 import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../Header/Header';
 import Welcome from '../Welcome/Welcome';
 import Word from '../Word/Word';
 import { Route, Switch } from 'react-router-dom';
 
-const App = () => {
-    return (
+class App extends Component {
+  constructor () {
+    super();
+    this.state= {
+      savedWords: [],
+      topScorers: []
+    }
+  }
+
+  render() {  
+  return (
       <main>
         <Header />
         <Switch>
           <Route path='/learn' render={() => {
             <Word />
+          }} />
+          <Route path='/saved' render={() => {
+            <SavedWords words={this.state.savedWords} />
+          }} />
+          <Route path='/topscorers' render={() => {
+            <TopScorers words={this.state.topScorers} />
           }} />
           <Route exact path='/' render={() => {
             return (
@@ -24,6 +39,8 @@ const App = () => {
       </main>
     );
   }
+}
+
 
 
 export default App;
