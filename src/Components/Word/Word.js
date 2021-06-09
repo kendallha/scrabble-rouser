8ca8746b0dc1dfdb972a83f87060c3ab3132b818
kendallha <kendallhaw@gmail.com>
2021-06-08 17:45:40 -0700

    Add return statement for Word component

--- src/Components/Word/Word.js
+++ src/Components/Word/Word.js
@@ -5,17 +5,34 @@ class Word extends Component {
     super();
     this.state = {
       word: 'WELCOME',
+      value: '14 points',
       error: ''
     }
   }
 
+  // getLetterValue = (letter) => {
+  //   switch (letter) {
+  //     case 'a':
+  //       value = 
+  //   }
+  // }
+
+
   render () {
-    const wordTiles = word.split('').map(letter => {
+    const wordTiles = this.state.word.split('').map(letter => {
       return (
-        <Letter tile={letter} />
+        <Letter tile={letter} value='2' />
       )
     });
-}
+    return(
+      <section className='new-word'>
+        <article className="word-display">{wordTiles}</article>
+        <h3>{this.state.value}</h3>
+        <button>Another word</button>
+        <button>Save</button>
+      </section>
+    )
+  }
 }
 
 export default Word;
