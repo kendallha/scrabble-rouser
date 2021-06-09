307dbb8e85fcbcff6194c37dfc4effc8bfa263ff
kendallha <kendallhaw@gmail.com>
2021-06-08 19:42:28 -0700

    Add React components

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <Link to='/'>
+        <h1 className='app-name'>Scrabble-rouser</h1>
+      </Link>
+      <NavLink to='/learn'>
+        <h2>Learn words</h2>
+      </NavLink>
+      <NavLink to='/saved'>
+        <h2>Saved words</h2>
+      </NavLink>
+      <NavLink to='/topscorers'>
+        <h2>Top-scorers</h2>
+      </NavLink>
+    </header>
+  )
+}
+
+export default Header;
