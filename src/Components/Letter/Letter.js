843aabb8a74de8025ccf87d359e59926b29548de
kendallha <kendallhaw@gmail.com>
2021-06-09 19:48:00 -0700

    Format word tiles

--- src/Components/Letter/Letter.js
+++ src/Components/Letter/Letter.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { letterValues } from '../../utilities/value-calculations';
-import './scrabble.css'
+import './scrabble.css';
+import './Letter.css';
 
 const Letter = ({ tile }) => {
   return(
-    <div class="scrabble-container">
-      <span class="scrabble-tile__letter">{tile}<span class="scrabble-tile__number">{letterValues[tile]}</span></span>
+    <div class='scrabble-wrapper' >
+      <div class="scrabble-container">
+        <span class="scrabble-tile__letter">{tile}<span class="scrabble-tile__number">{letterValues[tile]}</span></span>
+      </div>
     </div>
   )
 }
