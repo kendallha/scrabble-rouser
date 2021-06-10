0dcb7808ef4ab8d5cba382deed3311fec9b296a4
kendallha <kendallhaw@gmail.com>
2021-06-09 20:13:12 -0700

    Increase letter spacing in header

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -11,7 +11,7 @@ const Welcome = () => {
   return(
     <section className='welcome'>
       <div className='word-tiles'>{wordTiles}</div>
-      <p className='intro'>Are you ever in the midst of a tough Scrabble match, and you can't seem to think of any words that take advantage of that triple score spot? Need some new inspiration? Scrabblerouser helps you learn new 7-letter-or-less words to make you a Scrabble champ in no time.</p>
+      <p className='intro'>Are you ever in the midst of a tough Scrabble match, and you can't seem to think of any words that take advantage of that triple score spot? Need some new inspiration? Scrabble-rouser helps you learn new 7-letter-or-less words to make you a Scrabble champ in no time.</p>
       <button className='start'>Show me a word</button>
     </section>
   )
