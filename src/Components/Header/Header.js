e6cc6b253be54139151f385c1e11e4414b1b8ca2
kendallha <kendallhaw@gmail.com>
2021-06-09 18:09:10 -0700

    Add navlink cover and active states

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,14 +9,16 @@ const Header = () => {
         <h1 className='app-name'>Scrabble-rouser</h1>
       </Link>
       <nav className='nav'>
-        <NavLink to='/learn'>
-          <h2>Learn words</h2>
+        <NavLink to='/learn' activeStyle={{
+    borderBottom: '4px solid black'
+  }}>
+          <h2 className='nav-link'>Learn words</h2>
         </NavLink>
-        <NavLink to='/saved'>
-          <h2>Saved words</h2>
+        <NavLink to='/saved' >
+          <h2 className='nav-link'>Saved words</h2>
         </NavLink>
         <NavLink to='/topscorers'>
-          <h2>Top-scorers</h2>
+          <h2 className='nav-link'>Top-scorers</h2>
         </NavLink>
       </nav>
     </header>
