f0e44412bc175693708cb02b99987ede661798d2
kendallha <kendallhaw@gmail.com>
2021-06-10 10:34:21 -0700

    Add special error message for 429 due to frequent rate limiting

--- src/Components/Word/Word.js
+++ src/Components/Word/Word.js
@@ -27,15 +27,21 @@ class Word extends Component {
         })
         this.evaluateWordScore();
       } else {
-        console.log("NO WAY");
-        console.log(formattedWord);
         this.getNewWord();
       }
     } catch (error) {
-      console.log(error)
-      this.setState({error: "Something went wrong, please try again."})
+      if (error.message === '429') {
+        this.setState({error: "Whoa there, champ! We're having trouble keeping up. Give us a moment to catch up and then try again."})
+      } else {
+          this.setState({error: "Something went wrong, please try again."})
+      }
     }
   }
+
+  componentDidMount = async () => {
+    this.getNewWord();
+  }
+
 
   checkForAcronyms = (word) => {
     if (word.split('').includes('A') || word.split('').includes('E') || word.split('').includes('I') || word.split('').includes('O') || word.split('').includes('U') || word.split('').includes('Y')) {
@@ -43,10 +49,6 @@ class Word extends Component {
     } else {
       return true;
     }
-  }
-
-  componentDidMount = async () => {
-    this.getNewWord();
   }
 
   evaluateWordScore = () => {
@@ -64,14 +66,6 @@ class Word extends Component {
         return(
           <Error error={this.state.error} />
         )
-    // } else if (!this.state.value) {
-    //     return(
-    //       <div className='issue-wrapper'>
-    //         <h3 className='issue'>Oh no, we had a problem scoring this word. Let's try that again.</h3>
-    //         <button className='word-button' onClick={() => this.getNewWord()}>Another word</button>
-    //       </div>
-
-    //     )
     } else {
       const wordTiles = this.state.word.split('').map(letter => {
         return (
