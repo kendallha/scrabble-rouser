c9ed2a433cc78d620e2158b6aa5624180a58ae17
kendallha <kendallhaw@gmail.com>
2021-06-10 16:50:47 -0700

    Add Error prop types

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Error.css';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 const Error = ({ error }) => {
   return(
@@ -12,6 +12,10 @@ const Error = ({ error }) => {
       </Link>
     </div>
   )
+}
+
+Error.propTypes = {
+  error: PropTypes.string
 }
 
 export default Error;
