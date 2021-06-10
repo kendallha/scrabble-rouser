61f2e1dfe3ba5e870fb42755e2d19ad25529025c
kendallha <kendallhaw@gmail.com>
2021-06-10 16:56:52 -0700

    Fix console error by adding key

--- src/Components/WordList/WordList.js
+++ src/Components/WordList/WordList.js
@@ -15,7 +15,7 @@ const WordList = ({ words, message, removeWord, type }) => {
       })
     return (
       <div className='word-item' key={Math.random()}>
-        <article className='word'>{letters}</article> 
+        <article className='word' key={Math.random()}>{letters}</article> 
         <p className='score'>{getWordValue(word)} points</p>
         <i className="fas fa-times-circle fa-2x" onClick={() => removeWord(word, type)}></i>
       </div>
