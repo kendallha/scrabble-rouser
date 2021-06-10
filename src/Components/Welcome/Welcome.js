21915477f141912f73a767de1d81820e9caa23c8
kendallha <kendallhaw@gmail.com>
2021-06-09 20:07:07 -0700

    Style button on welcome page

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -10,9 +10,9 @@ const Welcome = () => {
     })
   return(
     <section className='welcome'>
-      <div class='word-tiles'>{wordTiles}</div>
-      <p>Are you ever in the midst of a tough Scrabble match, and you can't seem to think of any words that take advantage of that triple score spot? Need some new inspiration? Scrabblerouser helps you learn new 7-letter-or-less words to make you a Scrabble champ in no time.</p>
-      <button className='start'>Show me a word!</button>
+      <div className='word-tiles'>{wordTiles}</div>
+      <p className='intro'>Are you ever in the midst of a tough Scrabble match, and you can't seem to think of any words that take advantage of that triple score spot? Need some new inspiration? Scrabblerouser helps you learn new 7-letter-or-less words to make you a Scrabble champ in no time.</p>
+      <button className='start'>Show me a word</button>
     </section>
   )
 }
