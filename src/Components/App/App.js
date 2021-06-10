3e1de33b06c670209ae4fdb8e748cc48f94ab83a
kendallha <kendallhaw@gmail.com>
2021-06-09 17:10:45 -0700

    Add background color to page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
   render() {  
   return (
-      <main>
+      <main className='main'>
         <Header />
         <Switch>
           <Route exact path='/learn' render={() => {
