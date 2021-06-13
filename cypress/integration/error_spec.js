dc02a0d2be2f7d1264dacac090c1d610a3ed5034
kendallha <kendallhaw@gmail.com>
2021-06-13 10:58:37 -0700

    Add Letter spec test

--- cypress/integration/error_spec.js
+++ cypress/integration/error_spec.js
@@ -1,47 +1,24 @@
-describe('Words', () => {
+describe('RouteErrorHandling', () => {
   beforeEach(() => {
-    cy.intercept('https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun%2C%20verb%2C%20adjective%2C%20adverb&excludePartOfSpeech=abbreviation%2C%20conjunction%2C%20family-name%2C%20given-name%2C%20idiom%2C%20phrasal-prefix%2C%20proper-noun%2C%20proper-noun-plural%2C%20proper-noun-possessive&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=2&maxLength=5&api_key=1weum3dz1dl1kbq02ep9xe0jt8ewzzu5wyc5quxfa80kz7rw1', {
-      word: "peach"
-    })
-    cy.visit('http://localhost:3000/learn')
+      cy.visit('http://localhost:3000/')
   })
 
-  it('Should display a loading message while the word is being fetched', () => {
-    cy.get('.loading').should('have.text', 'Looking for a great new word...')
+  it('Should have a 404 page for a non-existent path entered', () => {
+    cy.visit('http://localhost:3000/giggles')
+      .get('.error').should('have.text', 'Error: 404 - Page not found.')
   })
 
-  it('Should display the fetched word', () => {
-    cy.get('.word-display').contains("P")
-      .get('.word-display').contains("E")
-      .get('.word-display').contains("A")
-      .get('.word-display').contains("C")
-      .get('.word-display').contains("H")
+  it('Should have a button to return to the main page from the 404 page', () => {
+    cy.visit('http://localhost:3000/giggles')
+      .get('.error').should('have.text', 'Error: 404 - Page not found.')
+      .get('.redirect').should('have.text', 'Back to main').click()
+      .url().should('eq', 'http://localhost:3000/')
   })
 
-  it('Should display the point total for the fetched word', () => {
-    cy.get('.points').should('have.text', '12 points')
-  })
-
-  it('Should have a button to get another word', () => {
-    cy.intercept('https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun%2C%20verb%2C%20adjective%2C%20adverb&excludePartOfSpeech=abbreviation%2C%20conjunction%2C%20family-name%2C%20given-name%2C%20idiom%2C%20phrasal-prefix%2C%20proper-noun%2C%20proper-noun-plural%2C%20proper-noun-possessive&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=2&maxLength=5&api_key=1weum3dz1dl1kbq02ep9xe0jt8ewzzu5wyc5quxfa80kz7rw1', {
-      word: "maze"
-    })
-      .get('.new').should('have.text', "Another word").click()
-      .get('.word-display').contains("M")
-      .get('.word-display').contains("A")
-      .get('.word-display').contains("Z")
-      .get('.word-display').contains("E")
-      .get('.points').should('have.text', '15 points')
-  })
-
-  it('Should have a button to Save a word', () => {
-    cy.get('.save').should('have.text', 'Save').click()
-      .visit('http://localhost:3000/saved')
-      .get('.word').contains('P')
-      .get('.word').contains("E")
-      .get('.word').contains("A")
-      .get('.word').contains("C")
-      .get('.word').contains("H")
+  it('Should route back to the homepage from any page if a user clicks on the app name', () => {
+    cy.visit('http://localhost:3000/saved')
+      .get('.app-name').click()
+      .url().should('eq', 'http://localhost:3000/')
   })
 })
 
---
+++ cypress/integration/letter_spec.js
@@ -0,0 +1,15 @@
+describe('Letter', () => {
+  beforeEach(() => {
+    cy.intercept('https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun%2C%20verb%2C%20adjective%2C%20adverb&excludePartOfSpeech=abbreviation%2C%20conjunction%2C%20family-name%2C%20given-name%2C%20idiom%2C%20phrasal-prefix%2C%20proper-noun%2C%20proper-noun-plural%2C%20proper-noun-possessive&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=2&maxLength=5&api_key=1weum3dz1dl1kbq02ep9xe0jt8ewzzu5wyc5quxfa80kz7rw1', {
+      word: "no"
+    })
+    cy.visit('http://localhost:3000/learn')
+  })
+
+  it('Should contain a number for the score of each letter of the word displayed', () => {
+    cy.get('.scrabble-tile__letter').contains('N')
+    cy.get('.scrabble-tile__number').contains('1')
+    cy.get('.scrabble-tile__letter').contains('O')
+    cy.get('.scrabble-tile__number').contains('1')
+  })
+})
